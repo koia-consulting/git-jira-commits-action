d1e9bd83d55ac2ba6278a9f9810dfed41e2d6bf3
Filip Jakubowski-Drzewiecki <filip.jakubowski@gmail.com>
2023-07-25 14:24:10 +0200

    Update index.js

--- .github/actions/extract-jira-keys/index.js
+++ .github/actions/extract-jira-keys/index.js
@@ -1,68 +1,36 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-function extractJiraKeys(text) {
-    const jiraKeyPattern = /\b[A-Z][A-Z0-9]*-[0-9]+\b/g;
-    const result = text.match(jiraKeyPattern);
-    return result || [];
-}
-
-function extractUniqueJiraKeys(keys) {
-    return [...new Set(keys)];
-}
-
 async function run() {
     try {
-        const githubToken = core.getInput('github-token', { required: true });
-        const filter = core.getInput('filter', { required: false }) || 'all';
-        const octokit = github.getOctokit(githubToken);
+        // Get the GitHub token from the input
+        const token = core.getInput('github-token', { required: true });
+        const filter = core.getInput('filter');
 
-        const context = github.context;
+        // Get the payload from the GitHub event
+        const { payload } = github.context;
 
-        const { owner, repo } = context.repo;
-        const { number } = context.issue;
+        // Extract the JIRA keys from the PR/commit/branch based on the filter value
+        let jiraKeys = [];
+        const regex = /\b[A-Z0-9]{1,10}-\d+\b/gi;
 
-        let prKeys = [];
-        let commitKeys = [];
-        let branchKeys = [];
-
-        let sha;
-        if(context.eventName === 'pull_request') {
-            const { data: pr } = await octokit.rest.pulls.get({
-                owner,
-                repo,
-                pull_number: number,
-            });
-
-            prKeys.push(...extractJiraKeys(pr.title));
-            branchKeys.push(...extractJiraKeys(pr.head.ref));
-
-            sha = pr.head.sha;
-        } else if (context.eventName === 'push') {
-            const branchName = process.env.GITHUB_REF.split('/').pop();
-            branchKeys.push(...extractJiraKeys(branchName));
-
-            sha = context.sha;
+        // Add checks and extraction logic for PR title, commit messages, and branch name
+        // This is an example for PR title:
+        if (filter === 'all' || filter === 'pr') {
+            const prTitle = payload.pull_request?.title;
+            const foundKeys = prTitle?.match(regex);
+            if (foundKeys) {
+                jiraKeys.push(...foundKeys);
+            }
         }
 
-        const { data: commitData } = await octokit.rest.repos.listCommits({
-            owner,
-            repo,
-            sha,
-        });
+        // Don't forget to do the same for commit messages and branch name
 
-        commitData.forEach((commit) => {
-            commitKeys.push(...extractJiraKeys(commit.commit.message));
-        });
+        // Convert all JIRA keys to uppercase and remove duplicates
+        jiraKeys = [...new Set(jiraKeys.map(key => key.toUpperCase()))];
 
-        let jiraKeys = [];
-        if (filter === 'all' || filter === 'pr') jiraKeys.push(...prKeys);
-        if (filter === 'all' || filter === 'commit') jiraKeys.push(...commitKeys);
-        if (filter === 'all' || filter === 'branch') jiraKeys.push(...branchKeys);
-
-        jiraKeys = extractUniqueJiraKeys(jiraKeys);
-
-        core.setOutput('jira-keys', jiraKeys.join(', '));
+        // Set the output
+        core.setOutput('jira-keys', jiraKeys.join(','));
     } catch (error) {
         core.setFailed(error.message);
     }
