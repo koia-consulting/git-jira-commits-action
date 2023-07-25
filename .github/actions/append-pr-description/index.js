31bcb775ec93eac8fd383ce1e5ed3b69348a8109
Filip Jakubowski-Drzewiecki <filip.jakubowski@gmail.com>
2023-07-25 18:40:54 +0200

    Update index.js

--- .github/actions/append-pr-description/index.js
+++ .github/actions/append-pr-description/index.js
@@ -1,12 +1,12 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-function removeSpecificText(str, startString, endString) {
-    var regex = new RegExp(startString + '.*?' + endString, 'g');
-    var result = str.replace(regex, '');
-
-    return result;
-}
+// function removeSpecificText(str, startString, endString) {
+//     var regex = new RegExp(startString + '.*?' + endString, 'g');
+//     var result = str.replace(regex, '');
+//
+//     return result;
+// }
 
 async function run() {
     try {
@@ -25,24 +25,36 @@ async function run() {
         const markerStart = '\n-----\n### JIRA Tickets in PR \n';
         const markerEnd = '\n--*--*--\n';
 
-        let body = pullRequest.body;
-        if (body === null) {
-            body = '';
-        }
-        else {
-            body += removeSpecificText(pullRequest.body, markerStart, markerEnd);
-        }
+        // let body = pullRequest.body;
+        // if (body === null) {
+        //     body = '';
+        // }
+        // else {
+        //     body += removeSpecificText(pullRequest.body, markerStart, markerEnd);
+        // }
 
-        body += markerStart;
-        body += issuesInfo;
-        body += markerEnd;
+        // body += markerStart;
+        // body += issuesInfo;
+        // body += markerEnd;
 
-        await octokit.pulls.update({
+        // await octokit.pulls.update({
+        //     owner: repo.owner,
+        //     repo: repo.repo,
+        //     pull_number: issue_number,
+        //     body: body
+        // });
+
+        let comment = markerStart;
+        comment += issuesInfo;
+        comment += markerEnd;
+
+        await octokit.pulls.createReview({
             owner: repo.owner,
             repo: repo.repo,
             pull_number: issue_number,
-            body: body
+            body: comment
         });
+
     } catch (error) {
         core.setFailed(error.message);
     }
