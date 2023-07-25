38ebaebc6d3b9931f16b18b70ac1786cb62851d3
Filip Jakubowski-Drzewiecki <filip.jakubowski@gmail.com>
2023-07-25 13:52:35 +0200

    update pr description

---
+++ .github/actions/append-pr-description/index.js
@@ -0,0 +1,29 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+    try {
+        const token = process.env.GITHUB_TOKEN;
+        const issuesInfo = process.env.ISSUES_INFO;
+        const { repo, issue: { number: issue_number } } = github.context;
+
+        const octokit = github.getOctokit(token);
+
+        const { data: pullRequest } = await octokit.pulls.get({
+            owner: repo.owner,
+            repo: repo.repo,
+            pull_number: issue_number
+        });
+
+        await octokit.pulls.update({
+            owner: repo.owner,
+            repo: repo.repo,
+            pull_number: issue_number,
+            body: `${pullRequest.body}\n\n${issuesInfo}`
+        });
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+run();
