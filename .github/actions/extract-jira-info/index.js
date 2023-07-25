38c70761c4330b8fd41401035e054f1b538a5342
Filip Jakubowski-Drzewiecki <filip.jakubowski@gmail.com>
2023-07-25 13:31:20 +0200

    Update index.js

--- .github/actions/extract-jira-info/index.js
+++ .github/actions/extract-jira-info/index.js
@@ -1,80 +1,57 @@
 const core = require('@actions/core');
+const github = require('@actions/github');
 const JiraClient = require('jira-connector');
-
-// Formatter function for Jira issue
-function formatIssue(issue, indent = 0) {
-    const indentation = " ".repeat(indent);
-    return `${indentation}Issue ${issue.key}: ${issue.summary} (Author: ${issue.author}, Status: ${issue.status})`;
-}
 
 async function run() {
     try {
-        const jiraHost = core.getInput('jira-host', { required: false });
-        const jiraToken = core.getInput('jira-token', { required: false });
-        const jiraEmail = core.getInput('jira-email', { required: false });
+        const jiraHost = core.getInput('jira-host', { required: true });
+        const jiraToken = core.getInput('jira-token', { required: true });
+        const jiraEmail = core.getInput('jira-email', { required: true });
         const jiraKeys = core.getInput('jira-keys', { required: true }).split(',');
-
-        if (!jiraHost) {
-            core.setFailed("Missing required secret: jira-host");
-            return;
-        }
-
-        if (!jiraToken) {
-            core.setFailed("Missing required secret: jira-token");
-            return;
-        }
-
-        if (!jiraEmail) {
-            core.setFailed("Missing required secret: jira-email");
-            return;
-        }
 
         const jira = new JiraClient({
             host: jiraHost,
             basic_auth: {
-                email: jiraEmail,
-                api_token: jiraToken,
-            },
+                base64: Buffer.from(`${jiraEmail}:${jiraToken}`).toString('base64')
+            }
         });
 
-        let issuesInfo = "";
+        const issues = [];
 
         for (const key of jiraKeys) {
-            const { fields } = await jira.issue.getIssue({ issueKey: key.trim() });
-
-            const issue = {
-                key: key.trim(),
-                summary: fields.summary,
-                status: fields.status.name,
-                author: fields.creator.displayName,
-                description: fields.description,
-                dependencies: []
-            };
-
-            // Fetch details for linked issues
-            for (const link of fields.issuelinks) {
-                const linkedIssueKey = link.outwardIssue ? link.outwardIssue.key : link.inwardIssue.key;
-                const linkedIssue = await jira.issue.getIssue({ issueKey: linkedIssueKey });
-
-                issue.dependencies.push({
-                    key: linkedIssue.key,
-                    summary: linkedIssue.fields.summary,
-                    status: linkedIssue.fields.status.name,
-                    author: linkedIssue.fields.creator.displayName,
-                });
-            }
-
-            // Format issue info
-            issuesInfo += formatIssue(issue) + "\n";
-            for (const dep of issue.dependencies) {
-                issuesInfo += formatIssue(dep, 4) + "\n"; // indent dependent issues by 4 spaces
-            }
+            const issue = await jira.issue.getIssue({ issueKey: key, fields: ['summary', 'status', 'creator', 'issuelinks'] });
+            issues.push(issue);
         }
 
-        core.setOutput('issues-info', issuesInfo);
+        const formattedIssues = formatIssues(issues);
+        core.setOutput('issues-info', formattedIssues);
+
     } catch (error) {
         core.setFailed(error.message);
     }
+}
+
+function formatIssueInfo(issue) {
+    const issueLink = `<a href="https://${process.env.JIRA_HOST}/browse/${issue.key}">${issue.key}</a>`;
+    return `${issueLink}: ${issue.fields.summary} (Author: ${issue.fields.creator.displayName}, Status: ${issue.fields.status.name})`;
+}
+
+function formatIssues(issues) {
+    let result = '';
+
+    issues.forEach(issue => {
+        result += `- ${formatIssueInfo(issue)}\n`;
+
+        issue.fields.issuelinks.forEach(link => {
+            if (link.outwardIssue) {
+                result += `  - ${formatIssueInfo(link.outwardIssue)}\n`;
+            } else if (link.inwardIssue) {
+                result += `  - ${formatIssueInfo(link.inwardIssue)}\n`;
+            }
+        });
+    });
+
+    return result;
 }
 
 run();
